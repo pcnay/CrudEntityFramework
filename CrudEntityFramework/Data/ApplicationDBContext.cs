2ec25be2c8a511081a896c89149d0269646c7f6d
Ramon Ortega <pcnay2003@gmail.com>
2020-11-24 18:05:18 -0800

    Se inicia con el Modelo de la tabal Usuario

---
+++ CrudEntityFramework/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Data
+{
+  public class ApplicationDbContext:DbContext
+  {
+    // Ahora es crear el constructir de la clase.
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+    }
+
+    // Creando el DbSet
+    // En esta aprte es donde se agregan las tablas que tiene la base de datos 
+    public DbSet<Usuario> Usuario { get; set; }
+      
+
+  }
+}
---
+++ CrudEntityFramework/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityFramework.Models
+{
+  public class Usuario
+  {
+    // Se agrega System.ComponentModel.DataAnnotations para que se active [key]
+    [Key] // Es la llave de la tabla, estos on "DataAnnotations"
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El Nombre es obligatorio")]
+    public string Nombre { get; set; }
+
+    [Required(ErrorMessage = "El Telefono es obligatorio")]
+    [Display (Name = "Telefono")]
+    public string Telefono { get; set; }
+
+    [Required(ErrorMessage = "El Celular es obligatorio")]
+    [Display(Name = "Celular")]
+    public string Celular { get; set; }
+
+    [Required(ErrorMessage = "El Email es obligatorio")]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+  }
+}
